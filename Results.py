7f7b151999c96fa71dca363ce2e817485cfc206b
kanishaPathy <83269240+kanishaPathy@users.noreply.github.com>
2025-04-18 00:27:11 +0100

    Update Results.py

--- Results.py
+++ Results.py
@@ -1,6 +1,8 @@
-import streamlit as st
 import pandas as pd
 import joblib
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 
 # Load model and data
